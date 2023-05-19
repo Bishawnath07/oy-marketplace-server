5aabf0421c8ea2a0cd85e0b53038e2afe2f242c9
Bishawnath Ray <bishawnathprogram07@gmail.com>
2023-05-19 12:48:17 +0600

    complete search , get by email and delete method complete

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -26,6 +26,55 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toyCollection = client.db('toyDB').collection('toys');
+
+    // to get all my toys
+    app.get('/myToys' , async(req , res) =>{
+      const cousor = toyCollection.find()
+      const result = await cousor.toArray();
+      res.send(result)
+    })
+
+    app.post('/toys' , async(req , res) =>{
+        const newToy = req.body;
+        const result = await toyCollection.insertOne(newToy)
+        res.send(result);
+    })
+
+
+
+    // to see all toys 
+    app.get("/allToys/:text" , async(req , res) =>{
+        console.log(req.params.text)
+        if(req.params.text == "Horse" || req.params.text == "dinosaur" || req.params.text == 'teddy bear') {
+            const result = await toyCollection.find({category: req.params.text }).toArray();
+            console.log(result)
+            return res.send(result)
+        }
+        const result = await toyCollection.find({}).toArray();
+        res.send(result)
+    })
+
+
+    app.get("/myToys/:email", async (req, res) => {
+        console.log(req.params.id);
+        const toys = await toyCollection
+          .find({
+            email: req.params.email,
+          })
+          .toArray();
+        res.send(toys);
+      });
+
+      // for delete toys method
+      app.delete('/myToys/:id' , async(req , res) =>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await toyCollection.deleteOne(query);
+        res.send(result);
+      })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
