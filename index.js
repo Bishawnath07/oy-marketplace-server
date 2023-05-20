c7f1cfe6f96379705889537aadeda9c2229ababd
Bishawnath Ray <bishawnathprogram07@gmail.com>
2023-05-21 02:11:45 +0600

    update cors

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 // middleware
-app.use(cors());
+const corsOptions ={
+  origin:'*',
+  credentials:true,
+  optionSuccessStatus:200,
+  }
+  app.use(cors(corsOptions))
 app.use(express.json());
 
 
